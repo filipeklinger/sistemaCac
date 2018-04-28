e83e39a03cdec2cdb2ed83e394458f2862f8bb31
Filipe <filipeklinger@gmail.com>
2018-04-28 18:11:03 -0300

    Criado tabela de constantes PHP

---
+++ control/constantes.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Constantes para a aplicação
+ * Created by Filipe
+ * Date: 28/04/18
+ * Time: 17:02
+ */
+const SIM = 1;
+const NAO = 0;
+const INVALIDO = -1;
+
+//niveis de acesso
+const ADMINISTRADOR = 1;
+const PROFESSOR = 2;
+const ALUNO = 3;
+const VISITANTE = 4;
