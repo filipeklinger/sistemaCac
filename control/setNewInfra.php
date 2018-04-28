36e10801a84963cfa9ecdf7730622e03ba02987d
Filipe <filipeklinger@gmail.com>
2018-04-28 18:33:22 -0300

    primeira versao cadastro de infraestrutura

---
+++ control/setNewInfra.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by Filipe
+ * Date: 28/04/18
+ * Time: 18:15
+ */
+
+include_once '../model/DatabaseOpenHelper.php';
+include 'constantes.php';
+class infraestrutura{
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function insertPredio(){
+        $nome = isset($_POST['nome']) ? $_POST['nome'] : INVALIDO;
+        $localizacao = isset($_POST['localizacao']) ? $_POST['localizacao'] : INVALIDO;
+        $params = array($nome,$localizacao);
+        $this->db->insert("nome,localizacao","predio",$params);
+    }
+
+    public function insertSala(){
+        $predioId = isset($_POST['predio_id']) ? $_POST['predio_id'] : INVALIDO;;
+        $nome = isset($_POST['nome']) ? $_POST['nome'] : INVALIDO;
+        $is_ativo = 1;
+        $params = array($predioId,$nome,$is_ativo);
+        $this->db->insert("predio_id,nome,is_ativo","sala",$params);
+    }
+}
+//recebe por GET o tipo da infraestrutura
+$tipo = $_GET['tipo'];
+$infra = new infraestrutura();
+if($tipo == 1){
+    $infra->insertPredio();
+}else if($tipo == 2){
+    $infra->insertSala();
+}else{
+    echo "Erro no tipo";
+}
+
