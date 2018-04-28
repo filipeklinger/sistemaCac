b47f34658bc5bc190c813e42b3a9cd9346dc126b
Filipe <filipeklinger@gmail.com>
2018-04-28 15:14:50 -0300

    Criada Logica de Login na classe PHP

--- control/validadorLogin.php
+++ control/validadorLogin.php
@@ -4,4 +4,28 @@
  * Date: 27/04/18
  * Time: 18:22
  */
-//TODO criar logica de controle de acesso
+include_once '../model/DatabaseOpenHelper.php';
+class login{
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function verifyUser($usuario,$senha){
+        //usuario retornado caso de erro no login
+        $user = "[{\"id_pessoa\":\"-1\",\"nome\":\"err\",\"nv_acesso\":\"-1\"}]";
+        //primeiro buscamos os usuarios possiveis
+        $usr = json_decode($this->db->select("senha,pessoa_id","login","usuario = ?",array($usuario)));
+        //depois vericamos se o usuario encontrado e a senha informada conferem
+        if(password_verify($senha,$usr[0]->senha)){
+            $user = $this->db->select("id_pessoa,nome,nv_acesso","pessoa","id_pessoa = ?",array($usr[0]->pessoa_id));
+        }
+        return $user;
+    }
+}
+
+$login = new login();
+echo $login->verifyUser("filipe",123);
+//no banco de dados essa senha esta gravada como
+//PASSWORD_BCRYPT: $2y$10$rPPCz9EYNmxCPKSB3vUERe/zQLX0ZdldkyrCppn6cSRsRn.n/9TQq
