e27480fd0f5f9917e09b1db967b4fb907eb925d0
Filipe <filipeklinger@gmail.com>
2018-04-29 11:36:29 -0300

    Scripts movidos para 'jsonParser.js'

---
+++ js/jsonParser.js
@@ -0,0 +1,50 @@
+//Recuperando as informações
+function ajaxLoadGET(destino,funcaoParse,corpo){
+    var body = $(corpo);
+    //colocando uma mensagem de load para o usuario
+    body.append('<div class="loader"></div>');
+    var xhttp = new XMLHttpRequest();//Objeto Ajax
+    xhttp.onreadystatechange = function(){//toda vez que mudar estado chama a funcao
+        if(xhttp.readyState === 4){//estado 4 é quando recebe a resposta
+            $(".loader").remove();
+            body.empty();
+            funcaoParse(this.responseText,body);
+            //body.append(this.responseText);
+
+        }
+    };
+    xhttp.open("GET", destino);
+    xhttp.send();
+
+}
+
+function jsonParsePredios(json,corpo) {
+    var objJson = JSON.parse(json);
+    for(var i in objJson){
+        corpo.append(
+            '<tr>' +
+            '<td class=\'col-md-5\'> '+ objJson[i].nome + '</td>' +
+            '<td class=\'col-md-5\'> '+ objJson[i].localizacao + '</td>' +
+            '<td class=\'col-md-1\'> '+ isAtivo(objJson[i].is_ativo) + '</td>' +
+            '<td class=\'col-md-1\'> <a href="#" class="btn btn-primary"><span class=\'glyphicon glyphicon-pencil\'></span></a> </td>' +
+            '</tr>');
+    }
+}
+
+function jsonParseSalas(json,corpo) {
+    var objJson = JSON.parse(json);
+    for(var i in objJson){
+        corpo.append(
+            '<tr>' +
+            '<td class=\'col-md-5\'> '+ objJson[i].sala + '</td>' +
+            '<td class=\'col-md-5\'> '+ objJson[i].predio + '</td>' +
+            '<td class=\'col-md-1\'> '+ isAtivo(objJson[i].is_ativo) + '</td>' +
+            '<td class=\'col-md-1\'> <a href="#" class="btn btn-primary"><span class=\'glyphicon glyphicon-pencil\'></span></a> </td>' +
+            '</tr>');
+    }
+}
+
+function isAtivo(num) {
+    if(num === '1') return "Sim";
+    else return "nao";
+}
