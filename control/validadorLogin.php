6cbfd4ba3453acf013d5f6d4e818cc5b5a8a2619
Filipe <filipeklinger@gmail.com>
2018-04-29 16:39:36 -0300

    Inserido login Funcional

--- control/validadorLogin.php
+++ control/validadorLogin.php
@@ -5,7 +5,7 @@
  * Time: 18:22
  */
 include_once '../model/DatabaseOpenHelper.php';
-class login{
+class validadorLogin{
     private $db;
 
     public function __construct(){
@@ -13,23 +13,36 @@ class login{
     }
 
     public function verifyUser($login, $senha){
-        //usuario retornado caso de erro no login
-        $user = "[{\"id_pessoa\":\"-1\",\"nome\":\"err\",\"nv_acesso\":\"-1\"}]";
         //primeiro buscamos os usuarios possiveis
         $usr = json_decode($this->db->select("senha,pessoa_id","login","usuario = ?",array($login)));
         //depois vericamos se o usuario encontrado e a senha informada conferem
         if($usr != null and password_verify($senha,$usr[0]->senha)){
-            $user = $this->db->select("id_pessoa,nome,nv_acesso","pessoa","id_pessoa = ?",array($usr[0]->pessoa_id));
+            $user = json_decode($this->db->select("id_pessoa,nome,nv_acesso","pessoa","id_pessoa = ?",array($usr[0]->pessoa_id)));
+            $_SESSION['LOGADO'] = true;
+            $_SESSION['USER'] = $user[0]->nome;
+            $_SESSION['NIVEL'] = $user[0]->nv_acesso;
+            $_SESSION['ID'] = $user[0]->id_pessoa;
+            $this->redireciona(true);
+        }else{
+            $_SESSION['LOGADO'] = false;
+            $this->redireciona(false);
         }
-        return $user;
+    }
+
+    private function redireciona($is_logado){
+        if($is_logado){
+            header("Location: ../index.php?pag=DashBoard");
+        }else{
+            header("Location: " . $_SERVER['HTTP_REFERER'] . "");//MANDA DE VOLTA PARA O login
+        }
     }
 }
 //Obtendo dados atraves de GET (para um debug mais facil)
-$login = isset($_GET['login']) ? $_GET['login'] : 'erro';
-$senha = isset($_GET['senha']) ? $_GET['senha'] : 0;
+$login = isset($_POST['login']) ? $_POST['login'] : 'erro';
+$senha = isset($_POST['senha']) ? $_POST['senha'] : 0;
 
-$objLogin = new login();
+$objLogin = new validadorLogin();
 
-echo $objLogin->verifyUser($login,$senha);
+$objLogin->verifyUser($login,$senha);
 //no banco de dados essa senha esta gravada como
 //PASSWORD_BCRYPT: $2y$10$rPPCz9EYNmxCPKSB3vUERe/zQLX0ZdldkyrCppn6cSRsRn.n/9TQq
