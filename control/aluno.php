3ce2130c1b09829f72b086f0cfb7d789dd854054
Filipe <filipeklinger@gmail.com>
2018-05-27 12:30:48 -0300

    Inserida proteção contra insercao duplicada em turma

--- control/aluno.php
+++ control/aluno.php
@@ -28,30 +28,48 @@ class aluno{
         $estaParticipando = $estaParticipando[0]->n;
 
         if($estaParticipando < 2){
+            //buscando se o aluno já esta cadasrado nessa turma
+            $jaNaTurma = json_decode($this->db->select("count(*) as n","aluno_turma","turma_id = ? and pessoa_id = ?",array($idTurma,$idPessoa)));
+            $jaNaTurma = $jaNaTurma[0]->n;
+            if($jaNaTurma == null or $jaNaTurma == 0){
+                //obtendo num de vagas cadastradas na turma
+                $numVagas = json_decode($this->db->select("num_vagas","turma","id_turma = ?",array($idTurma)));
+                $numVagas = $numVagas[0]->num_vagas;
+                //inserindo aluno em turma
+                $mens = "";
+                if($numVagas > 0){
+                    //pessoas dentro do numero de vagas
+                    $params = array($idTurma,$idPessoa,NAO,SIM);
+                    //atualizamos o valor de vagas
+                    $paramsUpdate = array(($numVagas-1));
+                    $this->db->update(array("num_vagas"),"turma",$paramsUpdate,"id_turma = ?",array($idTurma));
+                }else{
+                    //pessoas na lista de espera
+                    $params = array($idTurma,$idPessoa,SIM,SIM);
 
-            //obtendo num de vagas cadastradas na turma
-            $numVagas = json_decode($this->db->select("num_vagas","turma","id_turma = ?",array($idTurma)));
-            $numVagas = $numVagas[0]->num_vagas;
-            echo "Vagas".$numVagas;
-            //inserindo aluno em turma
-            if($numVagas > 0){
-                $params = array($idTurma,$idPessoa,NAO,SIM);
-                //atualizamos o valor de vagas
-                $paramsUpdate = array(($numVagas-1));
-                $this->db->update(array("num_vagas"),"turma",$paramsUpdate,"id_turma = ?",array($idTurma));
+                    $this->db->select("count(*) as n","aluno_turma","id_turma = ? and lista_espera = ?",array($idTurma,));
+                    $mens = "na lista de espera ";
+                }
+
+                //tentando inserir o aluno na turma
+                if($this->db->insert("turma_id,pessoa_id,lista_espera,is_ativo","aluno_turma",$params)){
+                    new mensagem(SUCESSO,"Aluno inserido com sucesso ".$mens);
+                }else{
+                    new mensagem(INSERT_ERRO,"Não foi possivel inserir o aluno");
+                }
             }else{
-                $params = array($idTurma,$idPessoa,SIM,SIM);
+                new mensagem(ERRO,"Aluno Já está nessa turma");
             }
-            if($this->db->insert("turma_id,pessoa_id,lista_espera,is_ativo","aluno_turma",$params)){
-                new mensagem(SUCESSO,"Aluno inserido com sucesso");
-            }else{
-                new mensagem(INSERT_ERRO,"Não foi possivel inserir o aluno");
-            }
+
 
         }else{
             new mensagem(ERRO,"Aluno já esta participando de ".$estaParticipando." Oficinas");
         }
         $this->redireciona();
+    }
+
+    public function getAlunoByTurmaId($identificador){
+        //$this->db->select("")
     }
 
     private function redireciona(){header("Location: ../index.php?pag=Cad.Aluno");}
