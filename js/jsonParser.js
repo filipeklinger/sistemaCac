3b39d3a6503a48030cb00677335e740b6c8fe0dd
Vini <vinimartinsc@gmail.com>
2018-08-25 18:30:37 -0300

    modificações de estilo na busca, criação do manifest.json

--- js/jsonParser.js
+++ js/jsonParser.js
@@ -1003,6 +1003,7 @@ function trocaPag(pag) {
 }
 function trocaReq(req) {
     nivel=req;pagina=1;//reseta o numero de paginas
+    trocaPag(pagina);
     carregaUsuarios();
     //recarregando o paginador
     ajaxLoadGET('control/main.php?req=getPageNumber&nivel='+nivel, setPaginador, '#cadastrosRuralino');
